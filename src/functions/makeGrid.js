23521b394c7386c4f67dc9ee3e7c5f3cc890e30b
tahashieenavaz <tahashieenavaz@gmail.com>
2026-08-27 01:06:54 +0200

    fix: adjust grid line drawing logic and correct stroke color assignment

--- src/functions/makeGrid.js
+++ src/functions/makeGrid.js
@@ -5,14 +5,15 @@ export default function makeGrid(canvas, rows, cols) {
   const dx = canvas.height / cols;
   const dy = canvas.width / rows;
 
-  let y = 0;
+  let y = dy;
   for (let i = 0; i < rows - 1; i++) {
     context.beginPath();
     context.moveTo(0, y);
-    context.lineTo(canvas.width, y);
-    context.strokeColor = "white";
+    context.lineTo(canvas.width, -y);
+    context.strokeStyle = "white";
     context.stroke();
     context.closePath();
     y += dy;
+    console.log(dy, y, canvas.height);
   }
 }
