bed9d642ba1c23c7ea7290788520decade3bdd05
tahashieenavaz <tahashieenavaz@gmail.com>
2026-08-27 01:03:43 +0200

    fix: correct import path for get2DContext and enhance grid drawing logic

--- src/functions/makeGrid.js
+++ src/functions/makeGrid.js
@@ -1,8 +1,18 @@
-import get2DContext from "get2DContext";
+import get2DContext from "./get2DContext";
 
 export default function makeGrid(canvas, rows, cols) {
   const context = get2DContext(canvas);
   const dx = canvas.height / cols;
   const dy = canvas.width / rows;
-  console.log(dx, dy);
+
+  let y = 0;
+  for (let i = 0; i < rows - 1; i++) {
+    context.beginPath();
+    context.moveTo(0, y);
+    context.lineTo(canvas.width, y);
+    context.strokeColor = "white";
+    context.stroke();
+    context.closePath();
+    y += dy;
+  }
 }
