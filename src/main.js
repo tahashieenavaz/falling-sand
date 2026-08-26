5ceb4f6395af0df4fba895bfe38c44bc752e54dc
tahashieenavaz <tahashieenavaz@gmail.com>
2026-08-27 01:00:09 +0200

    fix: import makeGrid function in main.js for grid creation

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import "./style.css";
 import createCanvas from "@functions/createCanvas";
 import appendBody from "@functions/appendBody";
+import makeGrid from "@functions/makeGrid";
 
 const canvas = createCanvas();
 appendBody(canvas);
