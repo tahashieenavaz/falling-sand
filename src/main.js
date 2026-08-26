c92cd54bd4797c43a77ca1d538f6e8137c86fc02
tahashieenavaz <tahashieenavaz@gmail.com>
2026-08-27 01:59:29 +0200

    refactor: reduce grid size and optimize drawing logic in animation loop

--- src/main.js
+++ src/main.js
@@ -4,29 +4,46 @@ import appendBody from "@functions/appendBody";
 import makeGrid from "@functions/makeGrid";
 import isolate from "@functions/isolate";
 
-const ROWS = 100;
-const COLS = 100;
+const ROWS = 50;
+const COLS = 50;
 
 const canvas = createCanvas();
+const dx = canvas.width / COLS;
+const dy = canvas.height / ROWS;
+
 appendBody(canvas);
-makeGrid(canvas, ROWS, COLS);
+
+const whiteRectangles = [];
 
 canvas.addEventListener("mousedown", (event) => {
   const rect = canvas.getBoundingClientRect();
   const x = event.clientX - rect.left;
   const y = event.clientY - rect.top;
 
-  const dx = canvas.width / COLS;
-  const dy = canvas.height / ROWS;
-
   const indexX = Math.floor((x / canvas.width) * COLS);
   const indexY = Math.floor((y / canvas.height) * ROWS);
 
-  isolate(canvas, (context) => {
-    context.fillStyle = "white";
-    context.fillRect(indexX * dx, indexY * dy, dx, dy);
-    context.fill();
+  whiteRectangles.push([indexX, indexY]);
+});
+
+(function animate() {
+  isolate(canvas, (context) =>
+    context.clearRect(0, 0, canvas.width, canvas.height),
+  );
+  makeGrid(canvas, ROWS, COLS);
+  whiteRectangles.forEach((rectangle) => {
+    isolate(canvas, (context) => {
+      context.fillStyle = "white";
+      context.fillRect(rectangle[0] * dx, rectangle[1] * dy, dx, dy);
+      context.fill();
+    });
+    if (rectangle[1] < ROWS - 1) {
+      rectangle[1] += 0.5;
+      if (rectangle[1] > ROWS - 1) {
+        rectangle[1] = ROWS - 1;
+      }
+    }
   });
 
-  console.log(indexX, indexY);
-});
+  requestAnimationFrame(animate);
+})();
