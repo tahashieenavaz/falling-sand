f79949aa59b3d14fc19d348db0887365f306b19f
tahashieenavaz <tahashieenavaz@gmail.com>
2026-08-27 00:49:05 +0200

    fix: correct URL import usage in alias configuration

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from "vite";
-import { fileURLToPath } from "node:url";
+import { fileURLToPath, URL } from "node:url";
 
 export default defineConfig({
   resolve: {
     alias: {
-      "@": fileURLToPath(new URL("./src"), import.meta.url),
-      "@functions": fileURLToPath(new URL("./src/functions"), import.meta.url),
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+      "@functions": fileURLToPath(new URL("./src/functions", import.meta.url)),
     },
   },
 });
