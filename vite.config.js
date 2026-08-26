6ed4fda4129e07e5957aa297d27c6c43ca855696
tahashieenavaz <tahashieenavaz@gmail.com>
2026-08-27 00:42:37 +0200

    feat: adds functions alais

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src"), import.meta.url),
+      "@functions": fileURLToPath(new URL("./src/functions"), import.meta.url),
     },
   },
 });
