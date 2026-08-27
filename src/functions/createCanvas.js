cb00d003a21a9b3020217093655de9f0638659e8
tahashieenavaz <tahashieenavaz@gmail.com>
2026-08-27 02:09:38 +0200

    refactor: rename variable for clarity in createCanvas function

--- src/functions/createCanvas.js
+++ src/functions/createCanvas.js
@@ -4,8 +4,8 @@ export default function createCanvas(
   width = innerWidth / 2,
   height = innerWidth / 2,
 ) {
-  const $element = createElement("canvas");
-  $element.width = width;
-  $element.height = height;
-  return $element;
+  const element = createElement("canvas");
+  element.width = width;
+  element.height = height;
+  return element;
 }
